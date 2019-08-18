ec927be31a98ef94ba0660613767456b1ee71d18
Michael Holloway <mholloway@audiophonic.com>
2019-08-17 17:20:45 -0700

    updatey

--- dungeon_new/grid_passing.py
+++ dungeon_new/grid_passing.py
@@ -4,15 +4,138 @@ from random import randint, choice
 class TheGrid():
 	"""the grid object"""
 
-	def __init__(self):
-		self.row = 5
-		self.col = 5
-		self.matrix = self.make_grid()		# matrix is a list of lists
-
-	def make_grid(self):
-
+	def __init__(self, rowcol=5):
+		self.row = rowcol
+		self.col = rowcol
+		self.matrix = self.make_grid_lc()		# matrix is a list of lists
 		
+		self.marker = ' X '
+		self.matrix[2][2] = self.marker
+	
+	def make_grid(self):
+		"""non list-comp version of building the matrix, just for notes purposes"""
 
+		outer = []						# create the main list object itself
+		for x in range(self.row):		# create a list called inner. this will (eventually) happen five times (row = 5) 
+			inner = []
+			for y in range(self.col):	# but -each time- a list is created above, the following will also 
+										# happen before the next list is created...
+				inner.append(' * ')		# create a '*' character once for each value in range(col) - so, x5, and put them all in the current
+										# inner list (the one that just got created above).
+										# once those five * have been put in inner, we go -back- to the first for loop, create
+										# the next inner list, and so on...
+			outer.append(inner)			# once a single inner list is created and filled with 5 *'s, we put it in outer.
+
+		return outer
+
+	def make_grid_lc(self):
+		"""list comprehension version of making the matrix. so much cleaner!"""
+				    
+				    # This is the 'inner' loop	  And this is the 'outer' loop.
+                    #    |						   |
+		outer = [[' * ' for x in range(self.col)] for y in range(self.row)]
+		return outer
 
 	def print_grid(self):
+		for row in self.matrix:
+			for val in row:
+				print(val, end='')
+			print()
+		print()
+
+def outer_event():
+	"""game menu loop. gameplay is not active."""
+
+	active = True
+	possible_choices = [1, 2]   # input is converted to int at time of input, see below.
+
+	while active:
+
+		print('1. Start')
+		print('2. Quit')
+		print()
+
+		response = int(input('>'))
+
+		if response not in possible_choices:
+			print('You did not enter a valid command.')
+		else:
+			if response == 1:
+				main_event()
+			elif response == 2:
+				print('Thanks for using this fine software.')
+				active = False
+
+def main_event():
+	"""gameplay event loop. gameplay is active."""
+
+	active = True
+
+	grid_state = {'no_grid': True, 'rows_and_cols': 5}
+
+	grid = None		# just an explicit placeholder for the object, C style
+
+	while active:
+
+		if grid_state['no_grid']:
+			
+			grid = TheGrid(grid_state['rows_and_cols'])
+
+			print('Grid has been created')
+			grid_state['no_grid'] = False
+
+		print('1. Print Current Grid')
+		print('2. Modify Current Grid')
+		print('3. Make New Grid')
+		print('4. Print current grid id')
+		print('5. Return to Main Menu')
+		print()
+		
+		response = int(input('>'))
+
+		if response == 5:
+			print('Returning to Main Menu')
+			active = False
+
+		elif response == 1:
+			grid.print_grid()
+			
+		elif response == 2:
+			modify_grid(grid)
+
+		elif response == 3:
+			make_new_grid(grid_state)
+
+		elif response == 4:
+			print('The ID of the current grid is {}'.format(id(grid)))
+			print()
+
+def modify_grid(grid):
+
+	print('Enter new Row coordinate')
+	x = int(input('>'))
+	x -= 1
+	print('Enter new Col coordinate')
+	y = int(input('>'))
+	y -= 1
+
+	grid.matrix[2][2] = ' * '
+	grid.matrix[x][y] = ' X '
+
+def make_new_grid(grid_state):
+
+	print('How many columns and rows do you want the new grid to have?')
+	rows_and_cols = int(input('>'))
+
+	# package this new info so it gets used in main event loop where grid is created.
+	grid_state['rows_and_cols'] = rows_and_cols
+	grid_state['no_grid'] = True	# flip to true so grid creation occurs in the main_event while loop
+
+	print('New grid will be created. It will have {} rows and cols'.format(rows_and_cols))
+	
+
+
+
+outer_event()
+
 
---
+++ dungeon_new/grid_tracking_two.py
@@ -0,0 +1,171 @@
+def clear_screen():
+	"""simple command to clear the game screen"""
+	print("\033[H\033[J")
+
+class TheGrid():
+	"""the grid object"""
+
+	grid_counter = 0
+
+	def __init__(self, rowcol=5):
+		self.rowcol = rowcol
+		self.matrix = self.make_grid_listcomp()		# matrix is a list of lists
+		
+		self.marker = ' X '
+
+		self.__class__.grid_counter += 1
+		
+		self.previous_row = None
+		self.previous_col = None
+
+		self.current_row = int(rowcol/2)
+		self.current_col = int(rowcol/2)
+
+		self.matrix[self.current_row][self.current_col] = ' X ' # print the starting position of the X on init
+
+	def print_grid_counter(self):
+		print('The value of self.__class__.grid_counter is currently {}'.format(self.__class__.grid_counter))
+		print('The value of grid_counter (in class namespace) is currently {}'.format(TheGrid.grid_counter))
+
+	def make_grid_listcomp(self):
+		"""list comprehension for creating the matrix"""
+		outer = [[' * ' for x in range(self.rowcol)] for y in range(self.rowcol)]
+		return outer
+
+	def print_grid(self):
+		"""print it, ya dingus"""
+		for row in self.matrix:
+			for val in row:
+				print(val, end='')
+			print()
+		print()
+
+def outer_event():
+	"""program menu loop. main functional event loop is not active, but software is running."""
+	clear_screen()
+
+	active = True
+	possible_choices = ['1','2']
+
+	while active:
+
+		print('1. Start')
+		print('2. Quit')
+
+		response = input('\n>')
+
+		if response not in possible_choices:
+			print('You did not enter a valid command.')
+		else:
+			if response == '1':
+				main_event()
+			elif response == '2':
+				print('Thanks for using this fine software.')
+				active = False
+
+def press_enter():
+	prompt = '...'
+	input(prompt)
+
+def main_event():
+	"""program event loop. the software is active."""
+
+	active = True
+
+	grid_state = {'no_grid': True, 'rows_and_cols': 5}	# dict object, stores info and flag for building new grid objects.
+
+	grid = None		# just an explicit placeholder for the grid object
+
+	possible_choices = ['1','2','3','4','5','6']
+
+	while active:
+
+		clear_screen()
+		
+		if grid_state['no_grid']:	# flag check: are we making a new grid or using current one?
+			
+			grid = TheGrid(grid_state['rows_and_cols'])
+
+			print('A Grid object has been created\n')
+
+			grid_state['no_grid'] = False # we want to keep using the grid we made until user chooses to create a new one.
+
+		print('1. Print Current Grid')
+		print('2. Modify Current Grid')
+		print('3. Make New Grid')
+		print('4. Print current grid id')
+		print('5. Print grid_counter attribute.')
+		print('6. Return to Main Menu')
+		print()
+		
+		response = input('>')
+
+		if response not in possible_choices:
+			print('\nYou did not enter a valid option, try again.')
+
+		else:
+
+			if response == '6':
+				print('Returning to Main Menu')
+				active = False
+
+			elif response == '1':
+				grid.print_grid()
+				
+			elif response == '2':
+				modify_grid(grid)
+
+			elif response == '3':
+				make_new_grid(grid_state)
+
+			elif response == '4':
+				print('The ID of the current grid is {}'.format(id(grid)))
+				print()
+
+			elif response == '5':
+				grid.print_grid_counter()
+
+		press_enter()
+
+def modify_grid(grid):
+	"""mutate the current grid object. id of grid will remain the same after mutation"""
+
+	# store current coords to previous since they are about to be udpated
+
+	grid.previous_row = grid.current_row
+	grid.previous_col = grid.current_col
+
+	print('Enter new Row coordinate')
+	row = int(input('>'))
+	row -= 1							# we're assuming the user isn't starting from 0
+	print('Enter new Col coordinate')
+	col = int(input('>'))
+	col -= 1
+
+	grid.current_row = row
+	grid.current_col = col
+
+	grid.matrix[grid.previous_row][grid.previous_col] = ' * '
+	grid.matrix[grid.current_row][grid.current_col] = ' X '
+
+def make_new_grid(grid_state):
+	"""provide instructions for generation of new grid, which will have a new id, and will created in main_event loop"""
+
+	print('How many columns and rows do you want the new grid to have?')
+	response = input('>')
+
+
+	rows_and_cols = int(response)
+
+	# package this new info into grid_state dictionary so it gets used in main event loop (where grid is created).
+	grid_state['rows_and_cols'] = rows_and_cols
+	grid_state['no_grid'] = True	# flip to true so grid creation occurs in the main_event while loop
+
+	print('Ok, a new grid will be created when the main event loop runs next.')
+	print('It will have {} rows and cols'.format(rows_and_cols))
+	
+# run the program
+outer_event()
+
+
+
